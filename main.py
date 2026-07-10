a486aadd155ef23647739105d6140b2235c509ce
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-10 13:44:47 +0200

    ajout de la variable Result_SPEED pour stocker les effets des dés en fonction de la vitesse de chaque cheval

---
+++ main.py
@@ -0,0 +1,6 @@
+from pandas import DataFrame
+
+
+RESULTS_SPEED = DataFrame(data = [[0, 1, 1, 1, 2, 2], [0, 0, 1, 1, 1, 2], [0, 0, 1, 1, 1, 2], [-1, 0, 0, 1, 1, 1], 
+                                  [-1, 0, 0, 0, 1, 1], [-2, -1, 0, 0, 0, 1], [-2, -1, 0, 0, 0, "DQ"]], columns = list(range(1,7)))
+
